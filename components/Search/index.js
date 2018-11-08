67e4fb85e135b1d58f242d32931ecbb69e8df54d
Jenny Gebske <jennifer@gebske.de>
2018-11-08 20:10:00 +0100

    add infobutton and infobox

--- components/Search/index.js
+++ components/Search/index.js
@@ -6,6 +6,9 @@ import searchIcon from './img/searchIcon.svg'
 import closeIcon from './img/closeIcon.svg'
 import dropdownIcon from './img/dropdownIcon.svg'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
+
 export default class Search extends Component {
   static defaultProps = {
     isOnSmallScreen: false,
@@ -153,33 +156,73 @@ export default class Search extends Component {
     const { value, highlightedSuggestion, inputVisible } = this.state
     const nothingFound = suggestions && suggestions.length === 0
 
-    return <div class={`${className} ${style.searchWrapper} ${inputVisible ? style.inputVisible : ''}`}>
-      <form onSubmit={this.handleSubmit} onReset={this.handleReset} onKeyDown={this.handleKeyDown}>
-        <input
-          ref={elem => { this.inputElement = elem }}
-          type='text'
-          placeholder={placeholder}
-          value={value}
-          onInput={this.handleInput}
-          onFocus={this.handleFocus}
-          // onBlur={this.handleBlur}
-          autoComplete={'off'} />
-        { this.getButton() }
-      </form>
-      {suggestions &&
-        <ul className={style.resultList}>
-          {suggestions.map((suggestion, i) =>
-            <li
-              onClick={this.handleResultSelect(suggestion.value)}
-              className={highlightedSuggestion === i ? style.active : ''}>
-              <div className={style.inner}>{suggestion.label}</div>
-            </li>)
-          }
-        </ul>
-      }
-      {nothingFound &&
-        <p class={style.nothingFound}>{ nothingFoundText }</p>
-      }
+    return <div>
+      <div class={`${className} ${style.searchWrapper} ${inputVisible ? style.inputVisible : ''}`}>
+        <form onSubmit={this.handleSubmit} onReset={this.handleReset} onKeyDown={this.handleKeyDown}>
+          <input
+            ref={elem => { this.inputElement = elem }}
+            type='text'
+            placeholder={placeholder}
+            value={value}
+            onInput={this.handleInput}
+            onFocus={this.handleFocus}
+            // onBlur={this.handleBlur}
+            autoComplete={'off'} />
+          { this.getButton() }
+        </form>
+        {suggestions &&
+          <ul className={style.resultList}>
+            {suggestions.map((suggestion, i) =>
+              <li
+                onClick={this.handleResultSelect(suggestion.value)}
+                className={highlightedSuggestion === i ? style.active : ''}>
+                <div className={style.inner}>{suggestion.label}</div>
+              </li>)
+            }
+          </ul>
+        }
+        {nothingFound &&
+          <p class={style.nothingFound}>{ nothingFoundText }</p>
+        }
+      </div>
+      <div class={style.wrapper}>
+        <div class={style.infoWrapper}>
+          <button class={style.infoButton} onClick={this.toggleInfoText}>
+            <FontAwesomeIcon icon={faInfo} />
+          </button>
+          <div class={style.infoBox}>
+
+            <p>
+              Diese Infografik von rbb|24 soll möglichst schnell und transparent die aktuelle Stickstoffdioxid-Belastung in Berlin und Brandenburg darstellen. Dafür werden mit einem automatisierten Programm die offiziellen Messdaten alle 10 Minuten von den landeseigenen Webseiten in <a href='https://luftdaten.berlin.de/lqi' target='_blank'>Berlin</a> und <a href='https://luftdaten.brandenburg.de/home/-/bereich/aktuell' target='_blank'>Brandenburg</a> abgefragt.
+              Dort erfahren Sie auch die genauen Standorte der Messstationen.
+            </p>
+            <p>
+              Unsere Farbskala orientiert sich an bisherigen Stickoxid-Recherchen des rbb: Niedrige Belastung wird grün dargestellt. Höhere Belastung in orange, rot und schließlich schwarz. Die Sortierung und Farbe der Stationen spiegelt stets den aktuellsten, übermittelten Wert wieder.
+            </p>
+            <p>
+              Sollte aus irgendwelchen Gründen kein Messwert übermittelt werden, wird der fehlende Datensatz interpoliert. Die Ticks auf der Y-Achse zeigen den heutigen Minimal- und Maximalwert sowie den errechneten Mittelwert in Mikrogramm pro Kubikmeter Luft (µg/m³).
+            </p>
+            <p>
+              <b>Quellen:</b>
+              <div>
+                Berlin: Messungen durch die Senatsverwaltung Umwelt, Verkehr und Klimaschutz
+              </div>
+              <div>
+                Brandenburg: Messungen durch das Landesamt für Umwelt
+              </div>
+            </p>
+            <p>
+              <b>Credits:</b>
+              <div>Design: Manuel Reich</div>
+              <div>Programmierung: Jenny Gebske, Arne Schlüter</div>
+              <div>Projektmanagement: Götz Gringmuth-Dallmer</div>
+              <div>Konzept: Dominik Wurnig</div>
+            </p>
+
+          </div>
+        </div>
+      </div>
+
     </div>
   }
 }
